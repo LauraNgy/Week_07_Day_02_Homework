a9b95704dbd2e69d94eba37649c0420ec315008d
Laura Nagy <ngylaura@gmail.com>
2018-09-12 09:51:21 +0100

    refactors DOM element creation to a helper constructor

--- src/views/instrument_display.js
+++ src/views/instrument_display.js
@@ -1,4 +1,5 @@
 const PubSub = require('../helpers/pub_sub.js');
+const CreateAppend = require('../helpers/create_append.js');
 
 const InstrumentDisplay = function (element) {
   this.element = element;
@@ -11,26 +12,21 @@ InstrumentDisplay.prototype.bindEvents = function () {
   });
 };
 
-InstrumentDisplay.prototype.newElement = function (tagName, parentName) {
-  const newElem = document.createElement(tagName);
-  parentName.appendChild(newElem);
-  return newElem;
-};
+// InstrumentDisplay.prototype.newElement = function (tagName, parentName) {
+//   const newElem = document.createElement(tagName);
+//   parentName.appendChild(newElem);
+//   return newElem;
+// };
 
 InstrumentDisplay.prototype.render = function (instrument) {
   const parent = this.element;
   this.element.innerHTML = '';
-  console.log(parent);
-  const instrumentHead = this.newElement('h2', parent);
-  instrumentHead.textContent = instrument.name;
-  const instrumentDesc = this.newElement('p', parent);
-  instrumentDesc.textContent = instrument.description;
-  const instrumentsList = this.newElement('ul', parent);
-  instrumentsList.textContent = "Here's a list of instruments in this family:"
+  const instrumentHead = new CreateAppend('h2', instrument.name, parent);
+  const instrumentDesc = new CreateAppend('p', instrument.description, parent);
+  const instrumentsList = new CreateAppend('ul', "Here'a a list of instruments in this family:", parent);
   const instruments = instrument.instruments;
   instruments.forEach( (instrument) => {
-    const newInstrument = this.newElement('li', instrumentsList);
-    newInstrument.textContent = instrument;
+    const newInstrument = new CreateAppend('li', instrument, instrumentsList);
   });
 };
 
